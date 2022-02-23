296aa2a43728bb006cc478edba55120d0d89a06d
Dima Karpov <dima.karpov@justeattakeaway.com>
2022-02-24 00:25:39 +0200

    Add home controller + hello world

---
+++ HelloWorldBackend/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldBackend.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "Index")]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
