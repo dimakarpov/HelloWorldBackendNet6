f579da63706bd0612988670c8c644badb02f9dd2
Dima Karpov <dima.karpov@justeattakeaway.com>
2022-03-02 11:59:40 +0200

    Add simulated event model

---
+++ HelloWorldBackend/Models/EventSimulator/SimulatedEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloWorldBackend.Models.EventSimulator
+{
+    public class SimulatedEvent
+    {
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
