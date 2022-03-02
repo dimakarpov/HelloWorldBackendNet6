384292bc6e63aba04f9aa16ca3c7c361b3d02d81
Dima Karpov <dima.karpov@justeattakeaway.com>
2022-03-02 11:52:08 +0200

    New event simulator controller

---
+++ HelloWorldBackend/Controllers/EventSimulatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldBackend.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class EventSimulatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IEnumerable<> GetEventDates([FromQuery] DateTime lastPleadingDate, [FromQuery] DateTime preTrialDate)
+        {
+            return View();
+        }
+    }
+}
